c32f1e60eebe7b74d8bb25790e8aeb38408dee2d
Himanshu Verma <himanshuv309@gmail.com>
2025-09-07 14:10:34 +0530

    remove modules and add storage

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
-
-import dotenv from "dotenv";
 import { clerkWebhook } from './controllers/webbooks.js';
-dotenv.config();
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './config/cloudinary.js';
+
 
 //Initialize express
 const app = express();
 
 // connect to database
 await connectDB();
+await connectCloudinary();
  
 //Routes
 app.get('/', (req, res) => {
@@ -19,10 +21,11 @@ app.get('/', (req, res) => {
 });
 
 app.post('/clerk', express.json(), clerkWebhook);
+app.use('/api/educator', express.json(), educatorRouter)
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(clerkMiddleware());
 
 //Port
 const PORT = process.env.PORT || 5000;
