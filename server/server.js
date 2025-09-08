50e302e22b710830305d941bbde88b98d7e7f6ef
Himanshu <himanshuv309@gmail.com>
2025-09-08 19:19:07 +0530

    change  /

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(clerkMiddleware())
 app.get('/',(req,res)=> res.send("API working"))
 app.post('/clerk',express.json(),clerkWebhooks)
 app.use('/api/educator',express.json(),educatorRouter)
-app.use('/api/course',express.json(),courseRouter);
+app.use('api/course',express.json(),courseRouter);
 app.use('/api/user',express.json(),userRouter)
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks);
 
