1eb8ecb4c8c39e0e5e185a1fe8dc57a8285e88a5
Himanshu <himanshuv309@gmail.com>
2025-09-09 15:37:36 +0530

    edit  cors and file path

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
 import userRouter from './routes/userRoutes.js'
-import bodyParser from "body-parser";
 
 //initialize Express
 const app = express()
@@ -16,7 +15,6 @@ const app = express()
 //connect to database
 await connectDB()
 await connectCloudinary()
-
 
 const allowedOrigins = [
   "https://lms-frontend-three-phi.vercel.app",
@@ -28,22 +26,19 @@ app.use(cors({
   credentials: true
 }));
 
-//Middlewares
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(clerkMiddleware());
 
 //Routes
-app.get('/',(req,res)=> res.send("API working"))
-app.post('/clerk',express.json(),clerkWebhooks)
-app.use('/api/educator',express.json(),educatorRouter)
-app.use('/api/course',express.json(),courseRouter);
-app.use('/api/user',express.json(),userRouter)
-app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks);
+app.get('/', (req, res) => res.send("API working"));
+app.post('/clerk', clerkWebhooks);
+app.use('/api/educator', educatorRouter);
+app.use('/api/course', courseRouter);
+app.use('/api/user', userRouter);
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
 //Port
-
-const PORT =  process.env.PORT || 5000
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
